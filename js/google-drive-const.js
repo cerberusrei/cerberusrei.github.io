9e20820481143e65c7847247642f7f34788890cc
cerberusreijp <cerberusreijp@gmail.com>
2023-06-17 17:41:41 +0900

    Move constants for Google Drive operations to external file

---
+++ js/google-drive-const.js
@@ -0,0 +1,40 @@
+// Define the fields to fetch from the API
+// See https://developers.google.com/drive/api/v3/reference/files
+const IMAGE_MEDIA_FIELDS = {
+    'width': {},
+    'height': {},
+    'rotation': {},
+    'location': {}, // {"latitude": double, "longitude": double, "altitude": double}
+    'time': {},
+    'cameraMake': {},
+    'cameraModel': {},
+    'exposureTime': {},
+    'aperture': {},
+    'flashUsed': {},
+    'focalLength': {},
+    'isoSpeed': {},
+    'meteringMode': {},
+    'sensor': {},
+    'exposureMode': {},
+    'colorSpace': {},
+    'whiteBalance': {},
+    'exposureBias': {},
+    'maxApertureValue': {},
+    'subjectDistance': {},
+    'lens': {}
+};
+
+const FILE_FIELDS = {
+    'id': { hidden: true },
+    'name': {},
+    'mimeType': { hidden: true },
+    'createdTime': {},
+    'modifiedTime': {},
+    'size': {},
+    'hasThumbnail': { hidden: true },
+    'webViewLink': { hidden: true }
+};
+
+const FILE_FIELDS_CRITERIA =
+    Object.keys(FILE_FIELDS).join(',')
+    + ',imageMediaMetadata(' + Object.keys(IMAGE_MEDIA_FIELDS).join(',') + ')';
