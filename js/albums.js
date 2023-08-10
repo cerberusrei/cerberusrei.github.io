fc95e4377fe14482a1a1d39c58b68f535a31223c
cerberusreijp <cerberusreijp@gmail.com>
2023-08-10 10:54:10 +0900

    Update albums

--- js/albums.js
+++ js/albums.js
@@ -1,30 +1,10 @@
 const ALBUM_LIST = [
     {
-        id: '1MZHDWRY1KgcFWgzTxyNEYimz982Llk2U',
-        name: '20221211-サクラタウンよさこい2022～師走祭～'
+        id: '1WkE-bpGpc6OIq9LzCtgYi5sa-9PQTAdC',
+        name: '20230809 高知よさこい祭り 前夜祭'
     },
     {
-        id: '1ztSoNGDSXCWS-EKenpzgIRMmq38XQJpv',
-        name: '20221127-東京元気祭り 2022'
-    },
-    {
-        id: '1YVu9fL7bqQ4c8CkcyIt93idVnScq-K2x',
-        name: '20221113-町田夢舞生ッスイ祭 2022'
-    },
-    {
-        id: '1ytzgYQc_SBp-PRTmbwb24PXJ9pWRMHL5',
-        name: '20221015-横浜よさこい祭り 2022'
-    },
-    {
-        id: '1yrIXKE-5XKQOe7_pmLduVFynqpk_z68i',
-        name: '20221016-横浜よさこい祭り 2022'
-    },
-    {
-        id: '1eHo-uOU1LTHQKFJhhd8BpMwiF81NTYmk',
-        name: '20221008-原宿表参道元氣祭 スーパーよさこい 2022'
-    },
-    {
-        id: '1yrx_QkJ14QCjcHYYoO1p_8CdZRVOpPPT',
-        name: '20221009-原宿表参道元氣祭 スーパーよさこい 2022'
+        id: '1a13Yx4OS9F3S8sRNWC6r7FnL5KOxeDd2',
+        name: '20230809 土佐学生よさこい大会'
     }
 ];
